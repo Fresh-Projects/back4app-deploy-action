af6cc2fe101aac5101d2bb0d60c8f823722f1179
Benjamin Gardiner <benjamin@freshprojects.co.za>
2023-03-03 15:28:14 +0200

    2226: Test bash before committing, it saves time...

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@ source /root/.bashrc
 
 /app/script.exp $2
 
-if [[ "$3"]]
-then
+if ! [ -z "$3" ]; then
     echo "Custom Path: $3"
     cd $3
+else
+    echo "Default Path"
 fi
 
 b4a default "$1"
